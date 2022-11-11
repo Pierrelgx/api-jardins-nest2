08c9583ac28d71f5fa901f948e78b67ef2709c8f
loiskouninef <lkouninef@gmail.com>
2022-11-11 12:46:06 +0100

    should have run one migration before generating the second one

--- db/migrations/1668166461305-ProductMigration.ts
+++ db/migrations/1668166461305-ProductMigration.ts
@@ -7,13 +7,9 @@ export class ProductMigration1668166461305 implements MigrationInterface {
     await queryRunner.query(
       `CREATE TABLE "product" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "price" integer NOT NULL, "types" character varying NOT NULL, "picture" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_bebc9158e480b949565b4dc7a82" PRIMARY KEY ("id"))`,
     );
-    await queryRunner.query(
-      `CREATE TABLE "user" ("id" SERIAL NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`,
-    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE "user"`);
     await queryRunner.query(`DROP TABLE "product"`);
   }
 }
