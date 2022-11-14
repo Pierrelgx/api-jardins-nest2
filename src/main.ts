b3095f3251925db66a57948bee41ee77b7ffad82
loiskouninef <lkouninef@gmail.com>
2022-11-14 19:11:29 +0100

    sessions stored in DB (prevent prod memory leak)

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,25 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as session from 'express-session';
 import * as passport from 'passport';
+import pgSession = require('connect-pg-simple');
+import pg = require('pg');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const pgPool = new pg.Pool({
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT),
+    user: process.env.DATABASE_USER,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+  });
 
   app.use(
     session({
+      store: new (pgSession(session))({
+        pool: pgPool,
+        createTableIfMissing: true,
+      }),
       secret: process.env.SESSION_SECRET,
       resave: false,
       saveUninitialized: false,
