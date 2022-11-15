30c4f1fc37999cc6c3c94fb5e004b722e6985d65
loiskouninef <lkouninef@gmail.com>
2022-11-15 17:21:37 +0100

    bcrypt comparison for login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -7,8 +8,10 @@ export class AuthService {
 
   async validateUser(email: string, password: string): Promise<any> {
     const user = await this.usersService.findOneByEmail(email);
+    const hash = await bcrypt.hash(password, Number(10));
+    const isMatch = await bcrypt.compare(password, hash);
 
-    if (user && user.password === password) {
+    if (user && isMatch) {
       // const { password, email, ...rest } = user;
       return user;
     }
