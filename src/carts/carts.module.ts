91b43db46783183df6228bcecce169d8541a4fce
loiskouninef <lkouninef@gmail.com>
2022-11-30 15:15:46 +0100

    remove unused import module

--- src/carts/carts.module.ts
+++ src/carts/carts.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from 'src/products/entities/product.entity';
 import { User } from 'src/users/entities/user.entity';
-import { Order } from 'src/orders/entities/order.entity';
 import { Cart } from './entities/cart.entity';
 import { CartsService } from './carts.service';
 import { CartsController } from './carts.controller';
@@ -13,7 +12,7 @@ import { ProductsService } from 'src/products/products.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Cart, User, Order, Product, OrderProduct]),
+    TypeOrmModule.forFeature([Cart, User, Product, OrderProduct]),
     MailerModule,
   ],
   providers: [CartsService, UsersService, ProductsService],
