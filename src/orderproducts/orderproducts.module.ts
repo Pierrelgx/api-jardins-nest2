8354b8bffee5c0b072f6c8ce726ba6d5312672b5
loiskouninef <lkouninef@gmail.com>
2022-12-07 16:05:24 +0100

    remove unused user import & useless orderproducts controller

--- src/orderproducts/orderproducts.module.ts
+++ src/orderproducts/orderproducts.module.ts
@@ -1,27 +1,22 @@
 import { Module } from '@nestjs/common';
 import { OrderProductsService } from './orderproducts.service';
-import { OrderProductsController } from './orderproducts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrderProduct } from './entities/orderproduct.entity';
 import { Order } from 'src/orders/entities/order.entity';
 import { Product } from 'src/products/entities/product.entity';
 import { MailerModule } from 'src/mailer/mailer.module';
-import { User } from 'src/users/entities/user.entity';
 import { Cart } from 'src/carts/entities/cart.entity';
-import { UsersModule } from 'src/users/users.module';
 import { CartsModule } from 'src/carts/carts.module';
 import { ProductsModule } from 'src/products/products.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([OrderProduct, Order, Product, User, Cart]),
+    TypeOrmModule.forFeature([OrderProduct, Order, Product, Cart]),
     MailerModule,
-    UsersModule,
     CartsModule,
     ProductsModule,
   ],
   providers: [OrderProductsService],
-  controllers: [OrderProductsController],
   exports: [OrderProductsService],
 })
 export class OrderproductsModule {}
